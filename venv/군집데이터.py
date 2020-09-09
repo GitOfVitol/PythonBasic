e364fb7892f0d0b9f06d0460543d69d2449dd739
LAPTOP-JBSIO3B7\ss398 <ss3980@khu.ac.kr>
2020-09-10 00:46:36 +0900

    수정

---
+++ venv/군집데이터.py
@@ -0,0 +1,10 @@
+#list 자료형
+names = ["Vitol", "Yeji", "John", "David", "Steve"]
+print(names)
+print(names[0])
+names.append('comp')
+print(names)
+print(len(names))
+
+multi_names = [[100, 95 , 85 ,78 ,75], ["Vitol", "Yeji", "John", "David", "Steve"]]
+print(multi_names[0][0])
