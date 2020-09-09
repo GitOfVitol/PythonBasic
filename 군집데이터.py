2477b37748954743d91aad8511a625b1d25ebf73
LAPTOP-JBSIO3B7\ss398 <ss3980@khu.ac.kr>
2020-09-10 01:19:40 +0900

    군집데이터 조금수정, 함수

--- 군집데이터.py
+++ 군집데이터.py
@@ -8,3 +8,12 @@ print(len(names))
 
 multi_names = [[100, 95 , 85 ,78 ,75], ["Vitol", "Yeji", "John", "David", "Steve"]]
 print(multi_names[0][0])
+
+print('\n')
+score = [100, 95, 86]
+print(len(score))
+print(max(score))
+print(min(score))
+print(sum(score))
+
+
---
+++ 함수.py
@@ -0,0 +1,8 @@
+def add(x, y):
+    result = x + y
+    return result
+
+a=int(input("Enter your first number : "))
+b=int(input("Enter your second number : "))
+result=add(a,b)
+print(result)
